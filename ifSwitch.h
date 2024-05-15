73237c54645c722851f8fb26b26efc90b9918654
bd2720 <brendandeneen9@gmail.com>
2024-05-15 17:18:30 -0400

    add observations

--- ifSwitch.h
+++ ifSwitch.h
@@ -4,10 +4,12 @@
 #include <sys/time.h>
 
 // number of loop iterations for each test
-#define ITERS 1000000
+// ITERS >= 0
+#define ITERS 10000000
 
 // number of if and/or else statements, or switch cases.
-#define N 1000
+// N >= 2
+#define N 100
 
 // compute elapsed time between 2 timevals
 #define TIME(t1, t2) ((t2).tv_sec-(t1).tv_sec)+(((t2).tv_usec-(t1).tv_usec)/1e6)
